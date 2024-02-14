f5ff1057921be37ea4ced3842916111fda1b2a3e
ksvvReddy <vardhanreddy071094@gmail.com>
2024-02-14 15:17:27 +0530

    listener

--- src/test/java/listener/TestListener.java
+++ src/test/java/listener/TestListener.java
@@ -2,6 +2,7 @@ package listener;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
@@ -13,20 +14,22 @@ import util.Constants;
 
 public class TestListener implements ITestListener {
 
-    @Override
-    public void onTestFailure(ITestResult result) {
+	@Override
+	public void onTestFailure(ITestResult result) {
 
-    	Date currentDate=new Date();
-		String screenshotName=currentDate.toString().replace("", "-").replace(":", "-");
-		File SrcFile =(File) (((TakesScreenshot) result.getTestContext().getAttribute(Constants.DRIVER)).getScreenshotAs(OutputType.FILE));
-        File DestFile=new File(".//screenshot//"+screenshotName+".png"); 
-  		try {
+		String pattern = "yyyy-MM-dd HH:mm:ss";
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+		String date = simpleDateFormat.format(new Date());
+		String screenshotName = result.getMethod().getMethodName()
+				+"_"+date.replace(" ", "-").replace(":", "_").replaceAll("-", "_")+"_Fail";
+		File SrcFile = (File) (((TakesScreenshot) result.getTestContext().getAttribute(Constants.DRIVER))
+				.getScreenshotAs(OutputType.FILE));
+		File DestFile = new File(".//screenshot//" + screenshotName + ".png");
+		try {
 			FileUtils.copyFile(SrcFile, DestFile);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-       }
-    }
-
-
+	}
+}
